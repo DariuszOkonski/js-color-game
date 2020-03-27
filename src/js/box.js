6d63a79cbb31c5f74fb65bcf3aacae83b23cd5b0
Dariusz Okonski <darek200180@gmail.com>
2020-03-27 10:52:01 +0000

    remove this.visibility and changeVisible from Box class

--- src/js/box.js
+++ src/js/box.js
@@ -3,15 +3,10 @@ class Box {
     this.red = this.setColor();
     this.green = this.setColor();
     this.blue = this.setColor();
-    this.visibility = true;
   }
 
   setColor() {
     return Math.floor(Math.random() * 256);
-  }
-
-  changeVisible() {
-    this.visibility = false;
   }
 }
 
